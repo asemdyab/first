8ae2b2e0b78186149d61fc2a869f6478a77652ac
unknown <asmdyab112233@gmail.com>
2024-11-13 17:30:11 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+// إضافة تفاعل عند الضغط على زر "شراء الآن"
+document.getElementById("buy-button").addEventListener("click", function() {
+    alert  (" تم إضافة المنتج إلى سلة التسوق! شكراً على اختيارك لنا.");
+});
+document.getElementById("sss").addEventListener("click",function() {
+    alert  (" تم إضافة المنتج إلى سلة التسوق! شكراً على اختيارك لنا.");
+});
+
+    document.getElementById("909").addEventListener("click",function() {
+        alert  (" تم إضافة المنتج إلى سلة التسوق! شكراً على اختيارك لنا.");
+    });
+    
+ 
+
